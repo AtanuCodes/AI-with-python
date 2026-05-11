23993e2bd2176f1aa39c4bdade2954f1200b1aeb
AtanuCodes <das.atanu42914@gmail.com>
2026-05-11 20:27:32 +0200

    miniproject practice

---
+++ Practice-mini-project/mini_project01.py
@@ -0,0 +1,5 @@
+kettle_boled = 0
+if kettle_boled:
+    print("The kettle is boiled, you can make tea now.")
+else:
+    print("The kettle is not boiled, you cannot make tea now.")
---
+++ Practice-mini-project/mini_project02.py
@@ -0,0 +1,5 @@
+user_inp = input("what do you want to eat?")
+if user_inp == 'cookies' or user_inp == 'samosa':
+    print(f"order confirmed for you {user_inp}")
+else:
+    print("we don't have that item, please order something else")

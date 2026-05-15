74581844d8d960657986d2b9ddd6da817ee0d8f7
AtanuCodes <das.atanu42914@gmail.com>
2026-05-15 21:06:23 +0200

    loop practice

---
+++ loop-mini-projects/chai_batch.py
@@ -0,0 +1,5 @@
+# chai shop makes chai in every 15 min, stimulate 4 batchs?
+for batch in range(1,5):
+    print(f"Preparing chai for batch number #{batch}")
+
+    
---
+++ loop-mini-projects/tea_shop.py
@@ -0,0 +1,4 @@
+# Fr every customer in line print the serial token number
+
+for token in range(1,11):
+    print(f"Serving chai for the #{token}")

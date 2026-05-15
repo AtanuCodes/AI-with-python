7fe879de6b3eb3ef31bbe65d2f33a9eb3abfb33f
AtanuCodes <das.atanu42914@gmail.com>
2026-05-15 21:20:02 +0200

    enumurate

---
+++ loop-mini-projects/enumurate.py
@@ -0,0 +1,5 @@
+# create a tea board and each menu item should be numbered
+
+tea= ['Green', 'Herbal', 'Lemon', 'Milk', 'Mint']
+for idx, item in enumerate (tea, start=100):
+    print(f'{idx} -  {item} '' tea')
---
+++ loop-mini-projects/take_order.py
@@ -0,0 +1,5 @@
+# use list of names and serve order and print that
+
+order = ['Atanu','Chris','Krish']
+for name in order:
+    print(f'Order is ready for {name}') 
